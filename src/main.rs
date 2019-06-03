d7cc04b4f0bd2ce7875c57c03bb661e533eb3ee6
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-06-03 22:49:33 +1000

    basic read from stdin and convert .md -> .html

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use pulldown_cmark::{Parser, Options, html};
+use pulldown_cmark::{Event, Parser, Options, Tag, html};
 use std::io::{self, Read};
 
 fn get_sdtin() -> io::Result<String> {
@@ -7,19 +7,28 @@ fn get_sdtin() -> io::Result<String> {
     Ok(buffer)
 }
 
-fn slackify(markdown_input: String) {
+fn slackify(markdown_input: String) -> String {
     // Set up options and parser. Strikethroughs are not part of the CommonMark standard
     // and we therefore must enable it explicitly.
     let mut options = Options::empty();
     options.insert(Options::ENABLE_STRIKETHROUGH);
     let parser = Parser::new_ext(&markdown_input, options);
 
+    let parser = parser.map(|event| match event {
+        Event::Text(text) => Event::Text(text.replace("abbr", "abbreviation").into()),
+        Event::Start(Tag::Header(_)) => Event::Start(Tag::Strong),
+        Event::End(Tag::Header(_)) => Event::End(Tag::Strong),
+        _ => {
+            println!("{:?}", event);
+            return event;
+        }
+    });
+
     // Write to String buffer.
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
 
-    // Show the output is what we expected.
-    println!("{}", html_output);
+    html_output
 }
 
 fn main() {
@@ -28,6 +37,23 @@ fn main() {
     let input = get_sdtin();
     match input {
         Err(e) => println!("error receiving input: {:?}", e),
-        Ok(str) => slackify(str)
+        Ok(str) => {
+            let slacked = slackify(str);
+            println!("{}", slacked)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_header_to_bold() {
+        let input = "## This is a title".to_owned();
+        let actual = slackify(input);
+        let expected = "<strong>This is a title</strong>";
+        assert_eq!(actual, expected);
     }
 }
